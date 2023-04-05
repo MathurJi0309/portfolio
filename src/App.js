3d2821a6c4ab68bd5beb6e477fd182a014f1fb25
MathurJi <anileshjjssacademy84@gmail.com>
2023-04-05 23:02:44 +0530

    add the portfolio

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import Home from './component/Home';
 import NavBar from './component/NavBar';
 import MediaLinks from './component/MediaLinks';
+import About from './component/About';
+import Portfolio from './component/Portfolio';
+import Experience from './component/Experience';
 
 function App() {
   return (
     <div>
       <NavBar/>
       <Home/>
+      <About/>
+      <Portfolio/>
+      <Experience/>
       <MediaLinks/>
     </div>
   );
---
+++ src/component/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const About = () => {
+
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+        </div>
+        <p className="text-xl mt-20">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+
+        </p>
+        <br />
+        <p className="text-xl">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
